b56f542ed287467bb6757641c038fe8d728c113e
Bolaji <layenibolaji5@gmail.com>
2021-11-04 14:08:01 -0700

    final commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import QuoteBox from './components/QuoteBox'
+
+function App() {
+    
+    
+  return (
+    <div className="App" >
+      <QuoteBox />
+    </div>
+  );
+}
+
+export default App;
+
